293cccef936d508fc6d16f533434034e55e836fa
Iurie Guzun <iurie_guzun@yahoo.com>
2020-12-11 13:03:37 -0500

    Add Sliders

--- Affine/ContentView.swift
+++ Affine/ContentView.swift
@@ -41,10 +41,23 @@ struct Flower: Shape {
     }
 }
 struct ContentView: View {
+   @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+
     var body: some View {
-        Text("Hello, World!")
-    }
-}
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .stroke(Color.red, lineWidth: 1)
+
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+        }
+    }}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
